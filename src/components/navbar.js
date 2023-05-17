eab84f8128a8e58e9583195f1c84130b57819447
hiep <hiep.dao@leximit.com>
2023-05-17 16:51:41 +0700

    menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,90 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import styles from '@/styles/navbar.module.scss';
+import React, {useState} from 'react';
 
 export default function Navbar() {
+
+  // Toggle menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState('black');
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    if (isMenuOpen) {
+      if (backgroundColor === 'black') {
+        setBackgroundColor('blue');
+      } else if (backgroundColor === 'blue') {
+        setBackgroundColor('yellow');
+      } else {
+        setBackgroundColor('black');
+      }
+    }
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <Image
-          src="/images/logo.svg"
-          alt="Lexim Global"
-          className={styles.logoImg}
-          width={23}
-          height={23}
-        />
-      </div>
+    <>
+      <header className={`${styles[backgroundColor]} ${styles.header} ${isMenuOpen ? styles.open : ''} `}>
+        <div className={styles.logoContainer}>
+          <Image
+            src="/images/logo.svg"
+            alt="Lexim Global"
+            className={styles.logoImg}
+            width={23}
+            height={23}
+          />
+        </div>
 
-      <div className="menuBtn">
-        <div className="menu-bar top"></div>
-        <div className="menu-bar bottom"></div>
-      </div>
+        <div className={styles.menuBtn} onClick={toggleMenu}>
+          <div className={`${styles.menuBar} ${styles.top}`}></div>
+          <div className={`${styles.menuBar} ${styles.bottom}`}></div>
+        </div>
+      </header>
 
-    </header>
+      <div className={`${styles.menuPanel}`}>
+        <div className={styles.menuItem}>
+          <a href='/about'>
+            <span>ABOUT</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+
+        <div className={styles.menuItem}>
+          <a href='/technology'>
+            <span>TECHNOLOGY</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+
+        <div className={styles.menuItem}>
+          <a href='/partnerships'>
+            <span>PARTNERSHIPS</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+
+        <div className={styles.menuItem}>
+          <a href='/case-studies'>
+            <span>CASE STUDIES</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+
+        <div className={styles.menuItem}>
+          <a href='/testimonials'>
+            <span>TESTIMONIALS</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+
+        <div className={styles.menuItem}>
+          <a href='/contact'>
+            <span>CONTACT</span>
+            <div className={styles.bottomLine}></div>
+          </a>
+        </div>
+      </div>
+      {/* menuPanel */}
+
+    </>
   );
 }
