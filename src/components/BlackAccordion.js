52e391a6676b07cf332574c72ef64fe017f2e057
hiep <hiep.dao@leximit.com>
2023-05-26 18:15:02 +0700

    Technology - slick

--- src/components/BlackAccordion.js
+++ src/components/BlackAccordion.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import styles from '@/styles/greenAccordion.module.scss';
+import styles from '@/styles/blackAccordion.module.scss';
+import Image from "next/image";
 
-const GreenAccordion = ({
+const BlackAccordion = ({
     children, className, title,
     layout = 'normal', // columnContent
     prefixTitle = false,
@@ -27,7 +28,7 @@ const GreenAccordion = ({
             <svg className={styles.downArrow} width="16" height="31" viewBox="0 0 16 31" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M7.29289 30.7071C7.68342 31.0976 8.31658 31.0976 8.70711 30.7071L15.0711 24.3431C15.4616 23.9526 15.4616 23.3195 15.0711 22.9289C14.6805 22.5384 14.0474 22.5384 13.6569 22.9289L8 28.5858L2.34315 22.9289C1.95262 22.5384 1.31946 22.5384 0.928933 22.9289C0.538409 23.3195 0.538409 23.9526 0.928933 24.3431L7.29289 30.7071ZM7 4.37114e-08L7 30L9 30L9 -4.37114e-08L7 4.37114e-08Z"
-                fill="#000"/>
+                fill="#fff"/>
             </svg>
           }
         </div>
@@ -38,4 +39,4 @@ const GreenAccordion = ({
   );
 };
 
-export default GreenAccordion;
+export default BlackAccordion;
--- src/pages/technology/index.js
+++ src/pages/technology/index.js
@@ -1,11 +1,27 @@
 import Layout from "@/components/layout";
+import Accordion from "@/components/Accordion";
 import GreenAccordion from "@/components/GreenAccordion";
+import BlackAccordion from "@/components/BlackAccordion";
 import Footer from "@/components/footer";
+import React, { useEffect } from 'react';
+import Slider from "react-slick";
 
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import page from '@/styles/page.module.scss';
 import styles from '@/styles/pageTechnology.module.scss';
 
+
+
 export default function Technology() {
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    adaptiveHeight: true
+  };
 
   return (
     <Layout className='pageTechnology'>
@@ -14,22 +30,17 @@ export default function Technology() {
         <span>TECHNOLOGY</span>
       </div>
 
-      <div id='fullPage'>
+      <div className={`${styles.technologySection} ${page.pageWrap}`}>
+        <div className={styles.innerSection}>
 
-        <div className={`${styles.technologySection} ${page.pageWrap}`}>
-          <div className={styles.innerSection}>
+          <div className={styles.blackLine}></div>
 
-            <div className={styles.blackLine}></div>
-
-            <GreenAccordion title="CORE COMPETENCIES" showContent={false} useArrow={false} />
-
+          <Accordion title="CORE COMPETENCIES" layout='fullContent' useArrow={false}>
             <GreenAccordion prefixTitle='01.' title="PLATFORMS" layout='columnContent'>
-              <div>
-                <p>Windows</p>
-                <p>Android</p>
-                <p>iOS</p>
-                <p>Linux</p>
-              </div>
+              <p>Windows</p>
+              <p>Android</p>
+              <p>iOS</p>
+              <p>Linux</p>
             </GreenAccordion>
 
             <GreenAccordion prefixTitle='02.' title="CLOUD" layout='columnContent'>
@@ -60,18 +71,44 @@ export default function Technology() {
               <p>Automated Programmable Infrastructure</p>
               <p>Consolidate Hardware Resources</p>
             </GreenAccordion>
+          </Accordion>
 
-            <GreenAccordion title="SYSTEMS INTEGRATION" useArrow={false}>
-            </GreenAccordion>
+          <BlackAccordion title="SYSTEMS INTEGRATION" useArrow={false}>
 
-            <div className={styles.blackLine}></div>
 
-          </div>
+            <Slider {...settings}>
+              <div className={`section`}>
+                <div>Many companies use different IT solutions for different tasks, but as business functions expand, they become overwhelmed by disjointed tools that can’t share data with each other.
+                  That’s why Systems Integration is so important to connect these different pieces of technology into a single ecosystem.
+                </div>
+              </div>
+
+              <div className={`section`}>
+                <div>Systems Integration involves connecting business software, applications, and databases together so that data can be pushed and pulled between them. Businesses use many different
+                  systems for sales, accounting, inventory or fulfillment, but these systems don't always work together. System integration solves this problem by connecting different systems
+                  together.
+                </div>
+              </div>
+
+              <div className={`section`}>
+                <div>This allows businesses to automate tasks, like moving data between systems, getting notifications when certain things happen, and generating reports. By doing this, businesses can
+                  improve their performance, gain more insight into their operations, and make better decisions.
+                </div>
+              </div>
+
+              <div className={`section`}>
+                <div>System Integration can also help businesses use new technology without having to sunset their old systems, as well as connect to 3rd Party systems (such as PayPal to take
+                  payments). Businesses can even connect to other businesses like suppliers and retailers to have data flowing from end to end.
+                </div>
+              </div>
+            </Slider>
+
+          </BlackAccordion>
+
         </div>
-
-        <Footer/>
-
       </div>
+
+      <Footer/>
 
     </Layout>
   )
