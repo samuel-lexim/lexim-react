5de99218a65894c977f013ac3c70da0ece182511
hiep <hiep.dao@leximit.com>
2023-05-30 17:03:39 +0700

    Testimonials page

--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -16,6 +16,7 @@ export default function Testimonials() {
     dots: false,
     slidesToShow: 1,
     slidesToScroll: 1,
+    adaptiveHeight: true
   };
 
   return (
@@ -27,73 +28,39 @@ export default function Testimonials() {
 
       <div className={`${styles.testimonialsWrap}`}>
 
-        <section className={`${styles.testimonialsSlider}`}>
-          <Slider {...settings}>
+        <section className={`${styles.testimonialsSection}`}>
 
+          <div className={styles.statistical}>
+            <span className={styles.statNumber}>50+</span>
+            <span className={styles.statText}>Customers Served</span>
+          </div>
+          <div className={styles.statistical}>
+            <span className={styles.statNumber}>150</span>
+            <span className={styles.statText}>Systems Integrations</span>
+          </div>
+          <div className={styles.statistical}>
+            <span className={styles.statNumber}>3 months</span>
+            <span className={styles.statText}>Average Project Completion</span>
+          </div>
+          <div className={styles.statistical}>
+            <span className={styles.statNumber}>30%</span>
+            <span className={styles.statText}>Average Increase In Sales</span>
+          </div>
+
+          <Slider {...settings} className={styles.testimonialSlider}>
             <div className={styles.testimonial}>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>50+</span>
-                <span className={styles.statText}>Customers Served</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>150</span>
-                <span className={styles.statText}>Systems Integrations</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>3 months</span>
-                <span className={styles.statText}>Average Project Completion</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>30%</span>
-                <span className={styles.statText}>Average Increase In Sales</span>
-              </div>
-
-              <div className={`${styles._content} fw-300 lh-1`}>
+              <div className={`${styles._content} fw-300 lh-1_1`}>
                 <p>“<span className='fw-700'>InterTrend</span> is a global advertising agency with comprehensive creative, broadcast, and print capabilities. Their innovative commercials and online video campaigns are recognized worldwide for their impact and effectiveness. With the support of LEXIM, Intertrend not only built a stunning agency website, but also custom mobile apps to enhance their client partnerships. Intertrend highly recommends Lexim for any of your web or mobile app needs.”</p>
               </div>
             </div>
 
             <div className={styles.testimonial}>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>50+</span>
-                <span className={styles.statText}>Customers Served</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>150</span>
-                <span className={styles.statText}>Systems Integrations</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>3 months</span>
-                <span className={styles.statText}>Average Project Completion</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>30%</span>
-                <span className={styles.statText}>Average Increase In Sales</span>
-              </div>
-
               <div className={`${styles._content} fw-300 lh-1`}>
                 <p><span className='fw-700'>Silverts Adaptive Clothing & Footwear</span> is a leader in adaptive apparel designed to support caregivers, elderly, families and nursing homes.</p>
               </div>
             </div>
 
             <div className={styles.testimonial}>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>50+</span>
-                <span className={styles.statText}>Customers Served</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>150</span>
-                <span className={styles.statText}>Systems Integrations</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>3 months</span>
-                <span className={styles.statText}>Average Project Completion</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>30%</span>
-                <span className={styles.statText}>Average Increase In Sales</span>
-              </div>
-
               <div className={`${styles._content} fw-300 lh-1`}>
                 <p><span className='fw-700'>Infinity Scrubs</span> is designed for active, luxurious functionality in the medical profession. It's products expands to nurses and medical staff worldwide. </p>
                 <div className='pad20'></div>
@@ -102,54 +69,20 @@ export default function Testimonials() {
             </div>
 
             <div className={styles.testimonial}>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>50+</span>
-                <span className={styles.statText}>Customers Served</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>150</span>
-                <span className={styles.statText}>Systems Integrations</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>3 months</span>
-                <span className={styles.statText}>Average Project Completion</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>30%</span>
-                <span className={styles.statText}>Average Increase In Sales</span>
-              </div>
-
               <div className={`${styles._content} fw-300 lh-1`}>
                 <p><span className='fw-700'>Teletron USA Luxury & Lifestyle</span> is a business for various products that consist of electronics and appliances that advocate for a luxurious lifestyle of comfort and quality.</p>
               </div>
             </div>
 
             <div className={styles.testimonial}>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>50+</span>
-                <span className={styles.statText}>Customers Served</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>150</span>
-                <span className={styles.statText}>Systems Integrations</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>3 months</span>
-                <span className={styles.statText}>Average Project Completion</span>
-              </div>
-              <div className={styles.statistical}>
-                <span className={styles.statNumber}>30%</span>
-                <span className={styles.statText}>Average Increase In Sales</span>
-              </div>
-
               <div className={`${styles._content} fw-300 lh-1`}>
                 <p><span className='fw-700'>Cherokee Brazil</span> is a blog web page created for Cherokee Uniforms, that support dual languages in Portuguese and English.</p>
                 <div className='pad20'></div>
                 <p><span className='fw-700'>Cherokee Uniforms</span> is apparel that serves comfort and quality, and a leading manufacture of medical uniforms across multiple brands.</p>
               </div>
             </div>
-
           </Slider>
+
         </section>
 
       </div>
