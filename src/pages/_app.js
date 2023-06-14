6c177f02f8ac7978daaf0555ee688721961d4b80
hiep <hiep.dao@leximit.com>
2023-06-14 17:05:04 +0700

    change favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,7 +35,7 @@ export default function App({Component, pageProps}) {
         <meta name="og:title" content={metaData.title}/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.svg"/>
       </Head>
       <Component {...pageProps} />
     </>
