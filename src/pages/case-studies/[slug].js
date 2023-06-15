cabc648417e8376b3349a6ddb19615faa75ac0d1
hiep <hiep.dao@leximit.com>
2023-06-15 16:52:51 +0700

    case study font size

--- src/pages/case-studies/[slug].js
+++ src/pages/case-studies/[slug].js
@@ -19,16 +19,16 @@ export default function ArticlePage() {
     <Layout className='pageCaseStudies pageCaseStudiesSlug'>
       <PageTitle title='CASE STUDIES' />
 
-      <div className={`${styles.caseStudiesWrap}`}>
+      <div className={`${styles.caseStudiesWrap} ${page.pageWrap}`}>
 
         <div className={styles.caseArticle}>
           {article && (
             <>
-              <h1 className={`${styles.articleTitle} FontSui`}>{article.title}</h1>
+              <h1 className={`${styles.articleTitle} FontSui s46_120`}>{article.title}</h1>
 
               {article.overview !== '' &&
                 <div className={styles.property}>
-                  <div className={styles.leftHeading}><h2>Overview</h2></div>
+                  <div className={styles.leftHeading}><h2 className='s24_30'>Overview</h2></div>
                   <div className={styles.rightContent}
                        dangerouslySetInnerHTML={{__html: article.overview}}></div>
                 </div>
@@ -36,7 +36,7 @@ export default function ArticlePage() {
 
               {article.challenge !== '' &&
                 <div className={styles.property}>
-                  <div className={styles.leftHeading}><h2>Challenge</h2></div>
+                  <div className={styles.leftHeading}><h2 className='s24_30'>Challenge</h2></div>
                   <div className={styles.rightContent}
                        dangerouslySetInnerHTML={{__html: article.challenge}}></div>
                 </div>
@@ -44,7 +44,7 @@ export default function ArticlePage() {
 
               {article.process !== '' &&
                 <div className={styles.property}>
-                  <div className={styles.leftHeading}><h2>Process</h2></div>
+                  <div className={styles.leftHeading}><h2 className='s24_30'>Process</h2></div>
                   <div className={styles.rightContent}
                        dangerouslySetInnerHTML={{__html: article.process}}></div>
                 </div>
@@ -61,7 +61,7 @@ export default function ArticlePage() {
                     d="M0.29289 7.2929C-0.0976334 7.68342 -0.0976333 8.31659 0.292891 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07106 0.928935C7.68054 0.538411 7.04738 0.538411 6.65685 0.928936L0.29289 7.2929ZM31 7L0.999998 7L0.999998 9L31 9L31 7Z"
                     fill="black"/>
                 </svg>
-                <span>Back</span>
+                <span className='s26_42'>Back</span>
               </div>
             </Link>
           </div>
