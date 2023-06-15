6dae34fe61aefd7440d807b9781abc5176911ee6
hiep <hiep.dao@leximit.com>
2023-06-15 17:18:32 +0700

    add footer to contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,6 +6,7 @@ import PageTitle from "@/components/PageTitle";
 import page from '@/styles/page.module.scss';
 import styles from '@/styles/pageContact.module.scss';
 import {useEffect} from "react";
+import Footer from "@/components/footer";
 
 export default function Contact() {
 
@@ -26,30 +27,30 @@ export default function Contact() {
   return (
     <Layout className='pageContact'>
 
-      <PageTitle title='CONTACT' />
+      <PageTitle title='CONTACT'/>
 
-        <div className={`${styles.fullPageSection} ${page.pageWrap} section initNotRunEffect`}>
+      <div className={`${styles.fullPageSection} ${page.pageWrap} section initNotRunEffect`}>
 
-          <section className={`${styles.contactSection} `}>
-            <div className={styles.innerSection}>
-              <h2 className={`${styles.s40_64} _backInLeft  FontSui fw-500 lh-1`}>No problem is too big or small.</h2>
-              <div className='pad20'></div>
-              <h1 className={`${styles.s50_140} _backInRight FontSui fw-500 lh-1`}>We’re here to help</h1>
-              <div className='pad20'></div>
+        <section className={`${styles.contactSection} `}>
+          <div className={styles.innerSection}>
+            <h2 className={`${styles.s40_64} _backInLeft  FontSui fw-500 lh-1`}>No problem is too big or small.</h2>
+            <div className='pad20'></div>
+            <h1 className={`${styles.s50_140} _backInRight FontSui fw-500 lh-1`}>We’re here to help</h1>
+            <div className='pad20'></div>
 
-              <div className={`${styles._content} lh-1`}>
-                <div className={`_backInBottom`}>
-                  <p className='fw-500 s24_64 lh-1_2'>Book your free consult today.</p>
-                </div>
-                <div className='pad20'></div>
-                <CtaBlueButton className='_fadeOutIn' text='Schedule a Call' link='#'/>
+            <div className={`${styles._content} lh-1`}>
+              <div className={`_backInBottom`}>
+                <p className='fw-500 s24_64 lh-1_2'>Book your free consult today.</p>
               </div>
+              <div className='pad20'></div>
+              <CtaBlueButton className='_fadeOutIn' text='Schedule a Call' link='#'/>
             </div>
+          </div>
 
-          </section>
-        </div>
+        </section>
+      </div>
 
-
+      <Footer />
 
     </Layout>
   )
