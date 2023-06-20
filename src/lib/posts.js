a1ec95135bf38e8a10dfaa4e5c4c3dd8c7d70480
hiep <hiep.dao@leximit.com>
2023-06-20 17:04:45 +0700

    page about

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -5,7 +5,7 @@ import matter from 'gray-matter';
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 
-export function getSortedPostsData() {
+export function GetSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
