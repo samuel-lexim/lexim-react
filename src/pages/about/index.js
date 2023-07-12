9c4d4d62c3fa2e961a2a6522f0edcd57de1ad61e
hiep <hiep.dao@leximit.com>
2023-07-12 16:07:18 +0700

    review - about fix bug

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -111,7 +111,7 @@ export default function About() {
 
                 <section className={`${styles.aboutSection} ${page.pageWrapLeft}`}>
                   <div className={styles.innerSection}>
-                    <div className='_backInRight'>
+                    <div className=''>
                       <a href='/'>
                         <Image
                           src="/images/LEXIM.svg"
